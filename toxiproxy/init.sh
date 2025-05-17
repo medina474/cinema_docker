68b8e87967bd905e4cc2872495642ce20746acc9
medina5 <emmanuel.medina@univ-lorraine.fr>
2025-05-17 14:43:11 +0200

    inversion port toxiproxy

--- toxiproxy/init.sh
+++ toxiproxy/init.sh
@@ -5,8 +5,8 @@ set -e
 # proxy pour PostgREST
 curl -s -X POST http://toxiproxy:8474/proxies -H "Content-Type: application/json" -d '{
   "name": "postgrest",
-  "listen": "0.0.0.0:4000",
-  "upstream": "host.docker.internal:3000"
+  "listen": "0.0.0.0:3000",
+  "upstream": "host.docker.internal:4000"
 }'
 
 # latence 1s ± 0.5
@@ -20,11 +20,26 @@ curl -s -X POST http://toxiproxy:8474/proxies/postgrest/toxics -H "Content-Type:
   }
 }'
 
+# proxy pour postgraphile
+curl -s -X POST http://toxiproxy:8474/proxies -H "Content-Type: application/json" -d '{
+  "name": "postgraphile",
+  "listen": "0.0.0.0:3001",
+  "upstream": "host.docker.internal:4001"
+}'
+
+# proxy pour php-crud-api
+curl -s -X POST http://toxiproxy:8474/proxies -H "Content-Type: application/json" -d '{
+  "name": "php-crud-api",
+  "listen": "0.0.0.0:3002",
+  "upstream": "host.docker.internal:4002"
+}'
+
+
 # proxy pour imgproxy
 curl -s -X POST http://toxiproxy:8474/proxies -H "Content-Type: application/json" -d '{
   "name": "imgproxy",
-  "listen": "0.0.0.0:4003",
-  "upstream": "host.docker.internal:3003"
+  "listen": "0.0.0.0:3003",
+  "upstream": "host.docker.internal:4003"
 }'
 
 # réduction de la bande passante à 25kbs⁻¹
